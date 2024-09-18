4d0be8cf67ec954018fa2b7c1fdabb0de186fb60
hoobooji <162422722+hoobooji@users.noreply.github.com>
2024-09-18 09:42:31 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,12 +10,12 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>○ Creator : <a href='t.me/nottylinks : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Update Channel : <a href='https://t.me/nottylinks'>Click here</a>\n○ Group : <a href='https://t.me/nottylinks'></a>\n○ Support Group : <a href='https://t.me/nottylinks'>Support</a></b>",
+            text = f"<b>○ Creator : <a href='https://t.me/Zenotty'> 𝖠𝖣𝖬𝖨𝖭 🗿</a>\n○ Channel : @nottyLeak\n○ Support Group : @ 𝖲𝖮𝖮𝖭 </b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
                     [
-                        InlineKeyboardButton("🔒 Close", callback_data = "close")
+                        InlineKeyboardButton("𝖢𝗅𝗈𝗌𝖾 👀", callback_data = "close")
                     ]
                 ]
             )
