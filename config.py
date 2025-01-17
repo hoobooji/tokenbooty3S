e14ee18ba045d3609959ab4976fd572d87288140
hoobooji <162422722+hoobooji@users.noreply.github.com>
2025-01-18 00:41:40 +0530

    Update config.py

--- config.py
+++ config.py
@@ -10,29 +10,29 @@ from logging.handlers import RotatingFileHandler
 
 
 #Bot token @Botfather
-TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "7551403768:AAHhs8_UcKTVfQHmoFWZkZQ_H1-KnrIwkWg")
+TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "75oFWZkZQ_H1-KnrIwkWg")
 
 #Your API ID from my.telegram.org
-APP_ID = int(os.environ.get("APP_ID", "22215048"))
+APP_ID = int(os.environ.get("APP_ID", "228"))
 
 #Your API Hash from my.telegram.org
-API_HASH = os.environ.get("API_HASH", "ed6b5f4819836d31defb5a747a1950ce")
+API_HASH = os.environ.get("API_HASH", "ed6b5f481a1950ce")
 
 #Your db channel Id
-CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002431384606"))
+CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-14606"))
 
 #OWNER ID
-OWNER_ID = int(os.environ.get("OWNER_ID", "7846069816"))
+OWNER_ID = int(os.environ.get("OWNER_ID", "78416"))
 
 #Port
 PORT = os.environ.get("PORT", "8026")
 
 #Database 
-DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://wtflinksofficial:wtflinksofficial@cluster0.1vvgr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-DB_NAME = os.environ.get("DATABASE_NAME", "wtflinksofficial")
+DB_URI = os.environ.get("DATABASE_URL", "mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+DB_NAME = os.environ.get("DATABASE_NAME", "")
 
-SHORTLINK_URL = os.environ.get("SHORTLINK_URL", "modijiurl.com")
-SHORTLINK_API = os.environ.get("SHORTLINK_API", "39d6d8e9bff7ac3b433bac744b8cb63962ef66a9")
+SHORTLINK_URL = os.environ.get("SHORTLINK_URL", "mo")
+SHORTLINK_API = os.environ.get("SHORTLINK_API", "39d6d8e9bff7ac3ba9")
 VERIFY_EXPIRE = int(os.environ.get('VERIFY_EXPIRE', 86400)) # Add time in seconds
 IS_VERIFY = os.environ.get("IS_VERIFY", "True")
 TUT_VID = os.environ.get("TUT_VID","nottyopen/7")
